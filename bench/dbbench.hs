c4d39444781daced212afd6dd0032bd289a2533d
Kyle Hanson <me@khanson.io>
2015-09-06 20:01:01 -0700

    bim

--- bench/dbbench.hs
+++ bench/dbbench.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE GADTs, TypeFamilies, TemplateHaskell, QuasiQuotes, FlexibleInstances, OverloadedStrings #-}
+{-# LANGUAGE GADTs, TypeFamilies, TemplateHaskell, QuasiQuotes, FlexibleInstances #-}
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad
 import Database.Groundhog.TH
 import Database.Groundhog.Sqlite
-import Data.Text (Text)
+import Data.Text (Text, pack)
 
 data Person = Person {name :: Text, age :: Int, height :: Int} deriving (Eq, Show)
 
@@ -14,16 +14,17 @@ mkPersist defaultCodegenConfig [groundhog|
 
 -- First, normal build, then profiling build with "-osuf p_o -hisuf p_hi"
 main :: IO ()
-main = withSqliteConn ":memory:" $ runDbConn $ do
---main = withPostgresqlConn "dbname=test user=test password=test host=localhost" $ runPostgresqlConn $ do
-  runMigration $ migrate (undefined :: Person)
-  let person = Person "abc" 22 180
-  k <- insert $ person
-  liftIO $ print k
-  b <- get k
-  liftIO $ print b
-  -- Do some math
-  result <- foldM (\acc a -> get k (\p -> return $ acc + (age p)))
-  sum foldMreplicateM_ 100000 $ get k --4.3
+main = withSqlitePool ":memory:" 10 $ \gCon -> runDbConn (do {ps <- replicateM 100 go; liftIO $ print (foldr (((+))) 0 ps)}) gCon
+  where go = do
+          runMigration $ migrate (undefined :: Person)
+          let person = Person (pack "abc") 22 180
+          k <- insert $ person
+          b <- get k
+          -- Do some math
+          (flip forM insert $ replicate 100 person)
+          rs <- selectAll
+          return (foldr (((+) . age . snd)) 0 rs)
+
+--  sum foldMreplicateM_ 100000 $ get k --4.3
 --  replicateM_ 10000 $ select $ AgeField ==. (22 :: Int)
 --  replicateM_ 100000 $ insert person
